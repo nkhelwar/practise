1cfe4ee9e68871940b50c923d2fec88954a3d57f
Neelanchal khelwar <nkhelwar@gmail.com>
2020-11-02 22:50:14 +0530

    Add files via upload

---
+++ MainApp.java
@@ -0,0 +1,19 @@
+package com.hsbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+
+@SpringBootApplication
+public class MainApp {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(MainApp.class, args);
+		
+		
+		
+		
+	}
+}
